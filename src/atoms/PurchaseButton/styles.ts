7a662f553fd7738d2eac510ba74d91098ae41df9
Guilherme Daros <128165190+guiDaros@users.noreply.github.com>
2025-10-07 01:00:54 +0000

    again...

--- src/atoms/PurchaseButton/styles.ts
+++ src/atoms/PurchaseButton/styles.ts
@@ -4,25 +4,12 @@ export const ButtonContainer = styled.button`
   background-color: #0066e8;
   border: none;
   border-radius: 0.5rem; /* leve arredondamento */
-  padding: 1rem 2rem;
   cursor: pointer;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-
-  transition: background-color 0.2s ease, transform 0.1s ease;
-  width: fit-content; /* se ajusta ao texto */
-  min-width: 12rem;
-  height: 3.5rem;
+  width: 200px;
+  height: 45px;
 
   &:hover {
     background-color: #3a86ff;
-  }
-
-  &:active {
-    transform: scale(0.98);
   }
 `;
 
@@ -32,5 +19,4 @@ export const ButtonText = styled.span`
   font-weight: 600;
   margin: 0;
   line-height: 1;
-  white-space: nowrap; /* evita quebra de linha */
 `;
