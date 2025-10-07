8fea6e9ab9686dad84cd7910d35335d3d1a07f1b
Guilherme Daros <128165190+guiDaros@users.noreply.github.com>
2025-10-07 00:50:34 +0000

    purchase button fix

--- src/atoms/PurchaseButton/styles.ts
+++ src/atoms/PurchaseButton/styles.ts
@@ -3,32 +3,34 @@ import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
   background-color: #0066e8;
-  text-align: center;
   width: 15rem;
   height: 4.7rem;
   border-radius: 1rem;
   font-size: 1.25rem;
   font-weight: 600;
   color: black;
-  overflow: hidden;
   cursor: pointer;
+
   display: flex;
   align-items: center;
   justify-content: center;
+  text-align: center;
+
   transition: transform 0.2s ease, background-color 0.2s ease;
 
   &:hover {
-    background-color: #3a86ff; /* Cor levemente mais escura */
-    transform: scale(1); /* Aumenta levemente o tamanho */
+    background-color: #3a86ff;
+    transform: scale(1.02);
   }
 
   &:active {
-    transform: scale(0.98); /* Reduz levemente o tamanho ao clicar */
+    transform: scale(0.98);
   }
 `;
 
 export const ButtonText = styled.p`
   text-align: center;
   color: #ffffff;
-  margin-botton: 10px;
+  margin-bottom: 0;
+  line-height: 1; 
 `;
